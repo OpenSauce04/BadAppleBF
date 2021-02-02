380451e1ce11a36b9bc5fcb09b40ef796aab5dfc
OpenSauce04 <danieljamesbradbury@hotmail.com>
2021-02-02 14:25:32 +0000

    Hid cursor

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ except:
   # Previous file does not exist, continue
   True # Empty instruction to stop python from complaining
 print("Compiling to './BadApple.bf'...")
-
+printtext(chr(27)+"[?25l") # Hide cursor
 printtext(chr(27)+"[2J") # Clear screen
 #storechar(" ")
 #writeraw(">")
@@ -22,9 +22,10 @@ printtext(chr(27)+"[2J") # Clear screen
 for x in range(1,3288):
   printfile("processedframes/"+str(x)+".txt")
   flushcode()
-printtext(chr(27)+"[f")
+printtext(chr(27)+"[f") # Reset cursor
 printtext("Demo Written by OpenSauce")
 printtext("Thanks for watching!")
+printtext(chr(27)+"[?25h") # Show cursor
 flushcode()
 glob.f.close()
 
